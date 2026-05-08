694b5983dcb753083151013f0f51ab23e76a8b93
darkhorse286 <darkhorse286@live.com>
2026-05-08 18:55:39 -0500

    EF Migration - CollectionId on Cards

---
+++ src/CardTrader.Infrastructure/Migrations/20260508235424_AddCollectionIdToCardInstance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardTrader.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollectionIdToCardInstance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "collection_id",
+                table: "card_instances",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "collection_id",
+                table: "card_instances");
+        }
+    }
+}
